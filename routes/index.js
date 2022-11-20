454d4da2a4f92d65183b2791b25c885b82183370
AllenliIE <allen.li.service@gmail.com>
2022-11-21 00:15:52 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const dashboardController = require('../controllers/dashboard-controller')
 const admin = require('./modules/admin')
+const dashboardController = require('../controllers/dashboard-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/dashboards', dashboardController.getDashboards)
 router.use('/', (req, res) => res.redirect('/dashboards'))
 
