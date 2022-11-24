ba4cc5838acbb386df2f3445596f3461b735fb06
AllenliIE <allen.li.service@gmail.com>
2022-11-24 19:49:04 +0800

    feat: flash msg&signup verification

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const dashboardController = require('../controllers/dashboard-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/dashboards', dashboardController.getDashboards)
 router.use('/', (req, res) => res.redirect('/dashboards'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
