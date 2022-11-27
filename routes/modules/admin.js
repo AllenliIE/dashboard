880f23ab98bc18d98c9dd6d3a4fcd4a6b3912aec
AllenliIE <allen.li.service@gmail.com>
2022-11-27 23:59:25 +0800

    feat: user model add is_admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth')
 
-router.get('/dashboards', adminController.getDashboards)
+router.get('/dashboards', authenticatedAdmin, adminController.getDashboards)
 router.use('/', (req, res) => res.redirect('/admin/dashboards'))
 
 module.exports = router
