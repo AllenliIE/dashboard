600e38f10f5458ac7c16708837dc06936d5a2fb2
AllenliIE <allen.li.service@gmail.com>
2022-11-30 20:26:16 +0800

    feat: admin create dashboard table

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -10,6 +10,19 @@ const adminController = {
     })
       .then(dashboards => res.render('admin/tables', { dashboards }))
       .catch(err => next(err))
+  },
+  createTable: (req, res) => {
+    return res.render('admin/create-table')
+  },
+  postTable: (req, res, next) => {
+    const { name, office, price, quantity, cost, date, owner } = req.body
+    if (!req.body) throw new Error('Order information is required')
+    Dashboard.create({ name, office, price, quantity, cost, date, owner })
+      .then(() => {
+        req.flash('success_messages', 'Order was successfully created')
+        res.redirect('/admin/tables')
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth')
 
-router.get('/tables', authenticatedAdmin, adminController.getTables)
-router.get('/dashboards', authenticatedAdmin, adminController.getDashboards)
+
+router.get('/tables/create', adminController.createTable)
+router.get('/tables', adminController.getTables)
+router.post('/tables', adminController.postTable)
+router.get('/dashboards', adminController.getDashboards)
 router.get('/', (req, res) => res.redirect('/admin/dashboards'))
 
 module.exports = router
